1a830bbd450df225e8bfff6a17ae9afda3782556
cclamb <cclamb@sandia.gov>
2017-03-27 16:11:32 -0600

    got it working with encryption

--- main.c
+++ main.c
@@ -4,6 +4,9 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/syslimits.h>
 
 #include <openssl/conf.h>
@@ -58,6 +61,11 @@ static int _load_write(void) {
     write(new_lib, libPrinter_dylib, sizeof(libPrinter_dylib));
     close(new_lib);
     return _load_and_run(FNAME);
+}
+
+static void _handle_error(const char* msg) {
+  perror(msg);
+  exit(FAIL);
 }
 
 static void _handleErrors(void) {
@@ -137,171 +145,34 @@ static int _unpack_library(
     /* Remove error strings */
     ERR_free_strings();
 
-    return 0;
+    return OK;
 }
 
 static int _load_packed(void) {
     unsigned char lib_shellcode[10000];
+
     _unpack_library(libPrinter_enc, sizeof(libPrinter_enc), lib_shellcode);
-    int new_lib = open(FNAME, O_CREAT | O_WRONLY);
-    write(new_lib, lib_shellcode, sizeof(lib_shellcode));
-    close(new_lib);
+
+    int new_lib = open(FNAME, O_CREAT | O_RDWR);
+    if (new_lib == -1) {
+      _handle_error("Error opening library file");
+    }
+
+    if (write(new_lib, lib_shellcode, sizeof(lib_shellcode)) == -1) {
+      _handle_error("Error writing library file");
+    }
+
+    if (close(new_lib) == -1) {
+      _handle_error("Error closing library file");
+    }
+
+    if (chmod(FNAME, S_IRWXU) == -1) {
+      _handle_error("Error changing library perms");
+    }
+
     return _load_and_run(FNAME);
 }
 
-
-
-
-
-
-
-#include <strings.h>
-
-void handleErrors(void)
-{
-  ERR_print_errors_fp(stderr);
-  abort();
-}
-
-int mencrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
-  unsigned char *iv, unsigned char *ciphertext)
-{
-  EVP_CIPHER_CTX *ctx;
-
-  int len;
-
-  int ciphertext_len;
-
-  /* Create and initialise the context */
-  if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
-
-  /* Initialise the encryption operation. IMPORTANT - ensure you use a key
-   * and IV size appropriate for your cipher
-   * In this example we are using 256 bit AES (i.e. a 256 bit key). The
-   * IV size for *most* modes is the same as the block size. For AES this
-   * is 128 bits */
-  if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
-    handleErrors();
-
-  /* Provide the message to be encrypted, and obtain the encrypted output.
-   * EVP_EncryptUpdate can be called multiple times if necessary
-   */
-  if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
-    handleErrors();
-  ciphertext_len = len;
-
-  /* Finalise the encryption. Further ciphertext bytes may be written at
-   * this stage.
-   */
-  if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) handleErrors();
-  ciphertext_len += len;
-
-  /* Clean up */
-  EVP_CIPHER_CTX_free(ctx);
-
-  return ciphertext_len;
-}
-
-int mdecrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
-  unsigned char *iv, unsigned char *plaintext)
-{
-  EVP_CIPHER_CTX *ctx;
-
-  int len;
-
-  int plaintext_len;
-
-  /* Create and initialise the context */
-  if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
-
-  /* Initialise the decryption operation. IMPORTANT - ensure you use a key
-   * and IV size appropriate for your cipher
-   * In this example we are using 256 bit AES (i.e. a 256 bit key). The
-   * IV size for *most* modes is the same as the block size. For AES this
-   * is 128 bits */
-  if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
-    handleErrors();
-
-  /* Provide the message to be decrypted, and obtain the plaintext output.
-   * EVP_DecryptUpdate can be called multiple times if necessary
-   */
-  if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
-    handleErrors();
-  plaintext_len = len;
-
-  /* Finalise the decryption. Further plaintext bytes may be written at
-   * this stage.
-   */
-  if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len)) handleErrors();
-  plaintext_len += len;
-
-  /* Clean up */
-  EVP_CIPHER_CTX_free(ctx);
-
-  return plaintext_len;
-}
-
-int aes_main (void)
-{
-  /* Set up the key and iv. Do I need to say to not hard code these in a
-   * real application? :-)
-   */
-
-  /* A 256 bit key */
-  unsigned char *key = (unsigned char *)"01234567890123456789012345678901";
-
-  /* A 128 bit IV */
-  unsigned char *iv = (unsigned char *)"0123456789012345";
-
-  /* Message to be encrypted */
-  unsigned char *plaintext =
-                (unsigned char *)"The quick brown fox jumps over the lazy dog";
-
-  /* Buffer for ciphertext. Ensure the buffer is long enough for the
-   * ciphertext which may be longer than the plaintext, dependant on the
-   * algorithm and mode
-   */
-  unsigned char ciphertext[128];
-
-  /* Buffer for the decrypted text */
-  unsigned char decryptedtext[128];
-
-  int decryptedtext_len, ciphertext_len;
-
-  /* Initialise the library */
-  ERR_load_crypto_strings();
-  OpenSSL_add_all_algorithms();
-  OPENSSL_config(NULL);
-
-  /* Encrypt the plaintext */
-  ciphertext_len = mencrypt (plaintext, strlen ((char *)plaintext), key, iv,
-                            ciphertext);
-
-  /* Do something useful with the ciphertext here */
-  printf("Ciphertext is:\n");
-  BIO_dump_fp (stdout, (const char *)ciphertext, ciphertext_len);
-
-  /* Decrypt the ciphertext */
-  decryptedtext_len = mdecrypt(ciphertext, ciphertext_len, key, iv,
-    decryptedtext);
-
-  /* Add a NULL terminator. We are expecting printable text */
-  decryptedtext[decryptedtext_len] = '\0';
-
-  /* Show the decrypted text */
-  printf("Decrypted text is:\n");
-  printf("%s\n", decryptedtext);
-
-  /* Clean up */
-  EVP_cleanup();
-  ERR_free_strings();
-
-  return 0;
-}
-
-
 int main(void) {
-    printf("Key size: %lu IV size: %lu\n", sizeof(KEY) * CHAR_BIT, sizeof(IV) * CHAR_BIT);
     return _load_packed();
-    //return aes_main();
 }
